fbb9425d6cdd581183f5b10025ff185cca2ce5cc
tarilabs <matteo.mortari@gmail.com>
2016-09-27 18:54:46 +0200

    how to test the ReactiveObjectUtil witout Mocking as it's all static methodS?

--- kie-maven-plugin/src/test/java/org/kie/maven/plugin/TempTest.java
+++ kie-maven-plugin/src/test/java/org/kie/maven/plugin/TempTest.java
@@ -1,5 +1,8 @@
 package org.kie.maven.plugin;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
@@ -10,24 +13,13 @@ import org.drools.core.phreak.ReactiveObject;
 import org.drools.core.phreak.ReactiveObjectUtil;
 import org.junit.Test;
 import org.mockito.Matchers;
-import static org.mockito.Mockito.*;
 
 import io.takari.maven.testing.executor.MavenExecutionResult;
-import io.takari.maven.testing.executor.MavenRuntime;
 
-public class InjectReactiveIntegrationTest extends KieMavenPluginBaseIntegrationTest {
-
-    public InjectReactiveIntegrationTest(MavenRuntime.MavenRuntimeBuilder builder) throws Exception {
-        super(builder);
-    }
-
+public class TempTest {
     @Test
     public void testBasicBytecodeInjection() throws Exception {
-        File basedir = resources.getBasedir("kjar-4-bytecode-inject");
-        MavenExecutionResult result = mavenRuntime
-                .forProject(basedir)
-                .execute("clean", "install");
-        result.assertErrorFreeLog();
+        File basedir = new File("/home/mmortari/git/droolsjbpm-integration/kie-maven-plugin/target/test-projects/InjectReactiveIntegrationTest_testBasicBytecodeInjection[3.0.5]_kjar-4-bytecode-inject");
         
         File classDir = new File(basedir, "target/classes");
         
@@ -37,13 +29,18 @@ public class InjectReactiveIntegrationTest extends KieMavenPluginBaseIntegration
         ClassLoader cl = new URLClassLoader( new URL[]{ classDir.toURI().toURL(), 
                 new File(BytecodeInjectReactive.classpathFromClass(ReactiveObject.class)).toURI().toURL() }, null );
         
-        Class<?> personClass = cl.loadClass("org.drools.compiler.xpath.tobeinstrumented.model.Adult");
+        Class<?> personClass = cl.loadClass("org.drools.compiler.xpath.tobeinstrumented.model.Person");
+        
+        for (Object c : personClass.getConstructors()) {
+            System.out.println(c);
+        }
         Constructor<?> personConstructor = personClass.getConstructor(String.class, int.class);
         Object personInstance = personConstructor.newInstance("Matteo", 34);
         Method setAgeMethod = personClass.getMethod("setAge", int.class);
         setAgeMethod.invoke(personInstance, 35);
         
-        
+        // FIXME well NO, mockito can't mock static method of ReactiveObjectUtil..
+        ReactiveObjectUtil mock = mock(ReactiveObjectUtil.class);
+        verify(mock).notifyModification(Matchers.any());
     }
-
 }
